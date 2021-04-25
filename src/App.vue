c8dca96ab86d6e42214932fd7a6ee7698fff3925
Ivo Minic <ivo.minic@gmail.com>
2021-04-25 21:21:22 +0200

    Made modal window

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <p></p>
+  <input type="text" ref="name" />
+  <button @click="toggleModal">Click me</button>
+  <div class="" v-if="showModal">
+    <Modal :header="header" :text="text" :arr="['first element', 4]" theme="sale" @closeEvent="toggleModal">
+      <template v-slot:links>
+        <a href="#">sign up</a>
+        <a href="#">more info</a>
+        <a href="#">about</a>
+      </template>
+      <h1>Slot title</h1>
+      <p class="">Slot text</p>
+    </Modal>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Modal,
+  },
+  data() {
+    return {
+      title: "My First Vue App",
+      header: "This is value passed as prop",
+      text: "Another prop value for text",
+      showModal: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      //console.log(this.$refs.name); //this.$refs provides access to any field with ref attribute
+      //console.log(this.$refs.name.value);
+      //this.$refs.name.classList.add("active");
+      //this.$refs.name.focus();
+      this.showModal = !this.showModal;
+    },
+  },
+};
 </script>
 
 <style>
@@ -22,5 +52,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <h1>{{ header }}</h1>
+      <p>{{ text }} - {{ arr[0] }} * {{ arr[1] }}</p>
+      <slot>Default content - only shows if we don't pass the slot</slot>
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  props: ["header", "text", "arr", "theme"],
+  methods: {
+    closeModal() {
+      this.$emit("closeEvent");
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+.modal h1 {
+  color: aqua;
+}
+.modal .actions {
+  text-align: center;
+  margin: 30px 0 10px 0;
+}
+.modal .actions a {
+  color: #333;
+  padding: 8px;
+  border: 1px solid white;
+  border-radius: 4px;
+  text-decoration: none;
+  margin: 10px;
+}
+.modal.sale {
+  background: crimson;
+  color: aliceblue;
+}
+.modal.sale h1 {
+  color: white;
+}
+.modal.sale .actions {
+  color: aliceblue;
+}
+.modal.sale .actions a {
+  color: white;
+}
+</style>
